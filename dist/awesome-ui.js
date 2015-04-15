a3a4df6ee68b3f68021144f375c6fbf7c20ba177
HopefulLlama <jonlaw10170@gmail.com>
2015-04-15 22:58:01 +0100

    Added direction, speed and refresh attrs to au-marquee

--- dist/awesome-ui.js
+++ dist/awesome-ui.js
@@ -1,20 +1,40 @@
 angular.module("awesome.ui", ["awesome.ui.marquee"]);
 angular.module('awesome.ui.marquee', [])
-  .directive('aumarquee', [function () {
+  .directive('auMarquee', [function () {
     return {
-      link: function (scope, element, attrs) {
-      	console.log(element);
-      	var offset = 0;
-      	element.context.style.paddingLeft = offset + "px";
-        var id = setInterval(function() {
-    		if(offset === window.innerWidth){
-    			offset = 0 - element.width();
-    		} else {
-    			offset++;
-    		}
-    		element.context.style.paddingLeft = offset + "px";
-        }, 10);
+      restrict:'E',
+      scope: {
+      	direction: '=',
+      	refresh: '=',
+      	speed: '='
       },
-      restrict:'E'
+      link: function (scope, element, attrs) {
+      	if(scope.direction == 'right') {
+      		scope.resetThreshold = window.innerWidth;
+      		scope.startPosition = 0 - element.width();
+      	} else if (scope.direction == 'left') {
+      		scope.resetThreshold = 0 - element.width();
+      		scope.startPosition = window.innerWidth;
+      	}
+      	
+      	scope.offset = 0;
+
+      	element.context.style.position = "relative";
+      	element.context.style.left = scope.offset + "px";
+        var id = setInterval(function() {
+    		if(scope.direction == 'right') {
+    			if(scope.offset >= scope.resetThreshold) {
+    				scope.offset = scope.startPosition;
+    			}
+    			scope.offset += scope.speed;
+    		} else if (scope.direction == 'left') {
+    			if(scope.offset <= scope.resetThreshold) {
+    				scope.offset = scope.startPosition;
+    			}
+    			scope.offset -= scope.speed;
+    		}	
+    		element.context.style.left = scope.offset + "px";
+        }, scope.refresh);
+      }
     };
   }]);
