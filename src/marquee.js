4305fbe68010aabf33a350e2839833704c90523e
HopefulLlama <jonlaw10170@gmail.com>
2015-04-16 03:17:03 +0100

    (fix) Issues 2: Now uses current window size to compare for position for resetting. (improvement): Removed refresh attribute

--- src/marquee.js
+++ src/marquee.js
@@ -4,36 +4,34 @@ angular.module('awesome.ui.marquee', [])
       restrict:'E',
       scope: {
       	direction: '=',
-      	refresh: '=',
       	speed: '='
       },
       link: function (scope, element, attrs) {
-      	if(scope.direction == 'right') {
-      		scope.resetThreshold = window.innerWidth;
-      		scope.startPosition = 0 - element.width();
-      	} else if (scope.direction == 'left') {
-      		scope.resetThreshold = 0 - element.width();
-      		scope.startPosition = window.innerWidth;
-      	}
-      	
       	scope.offset = 0;
 
       	element.context.style.position = "relative";
       	element.context.style.left = scope.offset + "px";
+
+      	scope.originalOffset = element[0].getBoundingClientRect().left;
+
         var id = setInterval(function() {
     		if(scope.direction == 'right') {
-    			if(scope.offset >= scope.resetThreshold) {
-    				scope.offset = scope.startPosition;
+    			if(element[0].getBoundingClientRect().left >= window.innerWidth) {
+					element.context.style.left = "0px";
+					scope.originalOffset = element[0].getBoundingClientRect().left;
+					scope.offset = 0 - scope.originalOffset - element.width();
     			}
     			scope.offset += scope.speed;
     		} else if (scope.direction == 'left') {
-    			if(scope.offset <= scope.resetThreshold) {
-    				scope.offset = scope.startPosition;
+    			if(element[0].getBoundingClientRect().left <= 0 - element.width()) {
+    				element.context.style.left = "0px";
+					scope.originalOffset = element[0].getBoundingClientRect().left;
+    				scope.offset = window.innerWidth - scope.originalOffset;
     			}
     			scope.offset -= scope.speed;
     		}	
     		element.context.style.left = scope.offset + "px";
-        }, scope.refresh);
+        }, 30);
       }
     };
   }]);
