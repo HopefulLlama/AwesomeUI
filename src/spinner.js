d55a400b195e0c727477eaf4e1a794c3b02f667a
Jonathan Law <jonlaw10170@gmail.com>
2017-03-25 19:41:36 +0000

    (fix) Removed default assignment of spinning because it didn't seem to work anyway

--- src/spinner.js
+++ src/spinner.js
@@ -9,21 +9,22 @@ angular.module('awesome.ui.spinner', [])
     transclude: true,
     template: '<span class="spinner-content" ng-transclude></span>',
     link: function (scope, element, attrs) {
-      if(scope.direction != "clockwise" || scope.direction != "anti-clockwise") {
-        scope.direction = "clockwise";
-      }
+      var DIRECTION = {
+        CLOCKWISE: "clockwise",
+        ANTI_CLOCKWISE: "anti-clockwise"
+      };
 
       scope.rotation = 0;
       scope.spinnerContent = element[0].querySelector('.spinner-content');
       scope.spinnerContent.style.display = "inline-block";
 
       var id = setInterval(function() {
-        if(scope.direction == 'clockwise') {
+        if(scope.direction == DIRECTION.CLOCKWISE) {
           if(scope.rotation >= 360) {
             scope.rotation = 0;
           }
           scope.rotation += scope.speed;
-        } else if (scope.direction == 'anti-clockwise') {
+        } else if (scope.direction == DIRECTION.ANTI_CLOCKWISE) {
           if(scope.rotation <= -360) {
             scope.rotation = 0;
           }
